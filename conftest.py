1b72d81c8f01911cbe3b839f5e080928a6ceb1dc
Natalya Mastyaeva <nata.m.754@gmail.com>
2023-10-04 20:56:03 +0400

    ref: conftest.py

--- conftest.py
+++ conftest.py
@@ -4,24 +4,24 @@ import allure
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
 
 
 @pytest.fixture(scope='function')
 def driver():
     print('\nstart browser...')
-    chrome_options = Options()
+    chrome_options = webdriver.ChromeOptions()
+    service = Service(executable_path=ChromeDriverManager().install())
     if 'CI' in os.environ:
         chrome_options.add_argument('--headless')
         chrome_options.add_argument('--no-sandbox')
         chrome_options.add_argument('--disable-dev-shm-usage')
-        driver = webdriver.Chrome(service=Service(), options=chrome_options)
+        driver = webdriver.Chrome(service=service, options=chrome_options)
         driver.set_window_size(1382, 754)
     else:
         chrome_options.add_argument('--headless')
         driver = webdriver.Chrome(service=Service())
         driver.maximize_window()
-        # driver.implicitly_wait(10)
     yield driver
     print('\nquit browser...')
     driver.quit()
