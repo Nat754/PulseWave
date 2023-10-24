2ba4f7bdb7ce93fc3ab721327662d55d53f563ff
Natalya Mastyaeva <nata.m.754@gmail.com>
2023-10-24 22:58:36 +0400

    add test for main_page

--- tests/test_main_page.py
+++ tests/test_main_page.py
@@ -18,55 +18,55 @@ class TestMainPage:
 
     @allure.title("Проверка перехода на страницу авторизации по кнопке 'Войти' в хедере")
     @pytest.mark.smoke
-    def test_redirect_auth_login(self, main_page_open, driver):
+    def test_get_header_auth_login_link(self, main_page_open, driver):
         main_page_open.get_header_auth_login().click()
         assert driver.current_url == LOGIN_PAGE, f"Произошел переход на страницу {driver.current_url}"
 
-    @allure.title(f"Проверка текста кнопки '{TEXT_LOGIN}' в хедере")
+    @allure.title("Проверка текста кнопки 'Войти' в хедере")
     @pytest.mark.smoke
-    def test_get_auth_login_text(self, main_page_open, driver):
+    def test_get_header_auth_login_text(self, main_page_open, driver):
         text = main_page_open.get_header_auth_login().text
         assert text == TEXT_LOGIN, f"Текст кнопки {text} не соответствует макету"
 
-    @allure.title(f"Проверка цвета кнопки '{TEXT_LOGIN}' в хедере")
+    @allure.title("Проверка цвета кнопки 'Войти' в хедере")
     @pytest.mark.smoke
-    def test_get_auth_login_color(self, main_page_open, driver):
+    def test_get_header_auth_login_color(self, main_page_open, driver):
         element = main_page_open.get_header_auth_login()
         color = element.value_of_css_property("background-color")
         assert color == BUTTON_COLOR, f"Цвет кнопки {TEXT_LOGIN} не соответствует макету"
 
     @allure.title("Проверка перехода на страницу регистрации по кнопке 'Регистрация' в хедере")
     @pytest.mark.smoke
-    def test_get_auth_signup_link(self, main_page_open, driver):
+    def test_get_header_auth_signup_link(self, main_page_open, driver):
         main_page_open.get_header_auth_signup().click()
         assert driver.current_url == SIGNUP_PAGE, f"Произошел переход на страницу {driver.current_url}"
 
-    @allure.title(f"Проверка текста кнопки '{TEXT_SIGNUP_HEADER}' в хедере")
+    @allure.title("Проверка текста кнопки 'Регистрация' в хедере")
     @pytest.mark.smoke
-    def test_get_auth_login_text(self, main_page_open, driver):
+    def test_get_header_auth_signup_text(self, main_page_open, driver):
         text = main_page_open.get_header_auth_signup().text
         assert text == TEXT_SIGNUP_HEADER, f"Текст кнопки {text} не соответствует макету"
 
-    @allure.title(f"Проверка цвета кнопки '{TEXT_SIGNUP_HEADER}' в хедере")
+    @allure.title("Проверка цвета кнопки 'Регистрация' в хедере")
     @pytest.mark.smoke
-    def test_get_auth_login_color(self, main_page_open, driver):
+    def test_get_header_auth_signup_color(self, main_page_open, driver):
         element = main_page_open.get_header_auth_signup()
         color = element.value_of_css_property("background-color")
         assert color == BUTTON_COLOR, f"Цвет кнопки {TEXT_SIGNUP_HEADER} не соответствует макету"
 
-    @allure.title("Проверка перехода на страницу регистрации по кнопке 'Регистрация' на странице")
+    @allure.title("Проверка перехода на страницу регистрации по кнопке 'Зарегистрироваться' на странице")
     @pytest.mark.smoke
     def test_get_body_auth_signup_link(self, main_page_open, driver):
         main_page_open.get_body_auth_signup().click()
         assert driver.current_url == SIGNUP_PAGE, f"Произошел переход на страницу {driver.current_url}"
 
-    @allure.title(f"Проверка текста кнопки '{TEXT_SIGNUP}' в хедере")
+    @allure.title("Проверка текста кнопки 'Зарегистрироваться' в хедере")
     @pytest.mark.smoke
     def test_get_body_auth_signup_text(self, main_page_open, driver):
         text = main_page_open.get_body_auth_signup().text
         assert text == TEXT_SIGNUP, f"Текст кнопки {text} не соответствует макету"
 
-    @allure.title(f"Проверка цвета кнопки '{TEXT_SIGNUP}' в хедере")
+    @allure.title("Проверка цвета кнопки 'Зарегистрироваться' в хедере")
     @pytest.mark.smoke
     def test_get_body_auth_signup_color(self, main_page_open, driver):
         element = main_page_open.get_body_auth_signup()
