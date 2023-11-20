b7d4263946e6f537f10e288192951787fcb5ef32
Natalya Mastyaeva <nata.m.754@gmail.com>
2023-11-20 14:44:30 +0400

    build new link

--- tests/constant.py
+++ tests/constant.py
@@ -1,6 +1,6 @@
 import datetime
 
-MAIN_PAGE = 'https://pulse-wave.netlify.app/'
+MAIN_PAGE = 'https://front.pwave.pnpl.tech/'
 MAIN_PAGE_HOME = f'{MAIN_PAGE}home'
 LOGIN_PAGE = f'{MAIN_PAGE}auth/login'
 SIGNUP_PAGE = f'{MAIN_PAGE}auth/signup'
