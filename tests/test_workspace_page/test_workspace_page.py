f0a67ac5b29a13ac944ecd6de0941bd861533cad
Natalya Mastyaeva <71374230+Nat754@users.noreply.github.com>
2024-01-21 01:34:20 +0400

    test: test_auth_user_redirect_to_workspace

---
+++ tests/test_workspace_page/test_workspace_page.py
@@ -0,0 +1,16 @@
+import allure
+import pytest
+from tests.constant import Constant
+
+
+@allure.epic(f"Тестирование страницы '{Constant.WORKSPACE}'")
+class TestWorkspacePage:
+    const = Constant
+
+    @allure.title("Проверка перехода в рабочее пространство авторизованного пользователя")
+    @pytest.mark.regress
+    def test_auth_user_redirect_to_workspace(self, auth_user, driver):
+        with allure.step('Авторизоваться пользователем'):
+            link = driver.current_url
+        with allure.step('Проверить переход в рабочее пространство'):
+            assert link == self.const.WORKSPACE, 'Не произошел переход в рабочее пространство'
