626eade369c4e8690ab292be1e2de0e3d49f564d
Natalya Mastyaeva <71374230+Nat754@users.noreply.github.com>
2024-03-06 22:08:52 +0400

    changed urls

--- tests/constant.py
+++ tests/constant.py
@@ -1,7 +1,7 @@
 class Constant:
 
-    MAIN_PAGE = 'https://front.pwave.pnpl.tech/'
-    # MAIN_PAGE = 'https://pulsewave.ru/'
+    # MAIN_PAGE = 'https://front.pwave.pnpl.tech/'
+    MAIN_PAGE = 'https://pulsewave.ru/'
     MAIN_PAGE_HOME = f'{MAIN_PAGE}home'
     LOGIN_PAGE = f'{MAIN_PAGE}auth/login'
     SIGNUP_PAGE = f'{MAIN_PAGE}auth/signup'
