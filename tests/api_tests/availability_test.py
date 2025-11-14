7c4f43b7a1c9c50137f5018468af4de40601ec1b
Natalya Mastyaeva <71374230+Nat754@users.noreply.github.com>
2025-11-14 20:11:39 +0400

    tests: availability_test.py

--- tests/api_tests/availability_test.py
+++ tests/api_tests/availability_test.py
@@ -4,7 +4,7 @@ import allure
 from api_testing.assertions import Assertions
 from tests.api_tests.api_constant import StatusCode
 from tests.constant import Links
-from data import TELEGRAM_TOKEN, my_chat
+from data import TELEGRAM_TOKEN, pulse_chat
 
 
 @allure.epic("Тестирование Доступности приложения")
@@ -20,11 +20,8 @@ class TestAvailability:
         try:
             response = requests.get(url)
             Assertions.assert_status_code(response, self.code.STATUS_OK)
-            url1 = f'https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage'
-            params = {'chat_id': my_chat, 'text': f'✅ *Сервер {url} доступен* ✅'}
-            response = requests.post(url1, data=params)
         except AssertionError:
-            url2 = f'https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage'
-            params = {'chat_id': my_chat, 'text': f'❌ *Сервер {url} недоступен* ❌'}
-            response = requests.post(url2, data=params)
+            url1 = f'https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage'
+            params = {'chat_id': pulse_chat, 'text': f'❌ *Сервер {url} недоступен* ❌'}
+            response = requests.post(url1, data=params)
             Assertions.assert_status_code(response, self.code.STATUS_OK)
